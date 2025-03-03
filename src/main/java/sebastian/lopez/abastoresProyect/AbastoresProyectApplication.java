a876b24f05b0fce82d791dd05fa21fd0bd0f6008
DeSebasWeb <juanselopezhernandez@gmail.com>
2025-03-03 15:28:45 -0500

    v0

---
+++ src/main/java/sebastian/lopez/abastoresProyect/AbastoresProyectApplication.java
@@ -0,0 +1,13 @@
+package sebastian.lopez.abastoresProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbastoresProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbastoresProyectApplication.class, args);
+	}
+
+}
