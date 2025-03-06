7042f1956c743ba79521383c9437af4515d15f63
DeSebasWeb <juanselopezhernandez@gmail.com>
2025-03-05 22:08:31 -0500

    i was updating the login.xhtml file and some things more

---
+++ src/main/java/sebastian/lopez/agriculturalManagementPlatform/controlador/LoginControlador.java
@@ -0,0 +1,10 @@
+package sebastian.lopez.agriculturalManagementPlatform.controlador;
+
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+public class LoginControlador implements Serializable {
+
+}
